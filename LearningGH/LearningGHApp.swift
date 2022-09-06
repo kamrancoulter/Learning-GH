a2d820b9f7df6c7ab0f44db7cadc5aee1c2f2284
Kamran Coulter <kamran.coulter@gmail.com>
2022-09-06 14:58:33 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Kamran Coulter on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
